1a34f722f777026b3f000d26ba82017ce808dad8
Keago99 <keatabde@gmail.com>
2026-04-09 17:02:29 +0200

    Alignment for Dialog
    Added alignment and CSS for starting Dialog
    Grid and flex combined for better viewing

--- displayController.js
+++ displayController.js
@@ -1,8 +1,8 @@
 // all code about displaying or updating gameboard goes here
 
 export function showDialogNames(){
-    const startingDialog = document.querySelector(".startingDialog");
+    const startingDialog = document.querySelector("#startingDialog");
 
-    startingDialog.showDialog();
+    startingDialog.showModal();
 }
 
